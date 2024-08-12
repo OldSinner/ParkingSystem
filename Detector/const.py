a06cb0b0ba3a13234cbe8012b24e22fe96765367
OldSinner <antylifepatia@gmail.com>
2024-08-12 21:40:26 +0200

    import sort and use for track

--- Detector/const.py
+++ Detector/const.py
@@ -1,5 +1,8 @@
 CAR_DETECTOR_MODEL = "./Detector/Models/yolov8n.pt"
 LICENCE_PLATE_MODEL = "./Detector/Models/license_plate_detector.pt" # https://github.com/Muhammad-Zeerak-Khan/Automatic-License-Plate-Recognition-using-YOLOv8
 VIDEO_PATH = "./TestData/testvideo.mp4"
-
 VEHICLES_IDS = [2,3,5,7]
+
+
+
+# https://github.com/abewley/sort/blob/master/sort.py object tracking
--- Detector/detector.py
+++ Detector/detector.py
@@ -1,23 +1,24 @@
 from ultralytics import YOLO
 from const import *
+from Detector.Sort.sort import *
 import cv2
 class Detector:
     def __init__(self) -> None:
         self.car_model = YOLO(CAR_DETECTOR_MODEL)
         self.license_playe = YOLO(LICENCE_PLATE_MODEL)
         self.cap = cv2.VideoCapture(VIDEO_PATH)
+        self.mot_tracker = Sort()
         pass
-
     def detect(self) -> None:
         ret = True
         frame_nr = -1
 
-        while ret and frame_nr < 10:
+        while ret :
             # Some preparing
             frame_nr += 1
             ret, frame = self.prepare_frame()
             # Detection
-            self.detectcar(frame)
+            cars = self.detect_cars(frame)
             #  Display 
             key = self.display(frame)
             if key == 27:
@@ -32,14 +33,20 @@ class Detector:
 
     def display(self, frame):
         cv2.imshow("Frame",frame)
-        key = cv2.waitKey(0)
+        key = cv2.waitKey(30)
         return key
     
-    def detectcar(self, frame) -> None:
+    def detect_cars(self, frame) -> list:
         detections = self.car_model(frame)[0]
         detections_ = []
         for detection in detections.boxes.data.tolist():
             x1, y1, x2, y2, score, class_id = detection
             if int(class_id) in VEHICLES_IDS:
                 detections_.append([x1, y1, x2, y2, score])
-                cv2.rectangle(frame,(int(x1),int(y1)),(int(x2),int(y2)),(0,255,0),2)
+        return detections_
+    
+    def track(self,list) -> np.NDArray:
+       return self.mot_tracker.update(np.asarray(list))
+    
+    def find_plates(self,frame) -> None:
+        pass
