4d20af9adf685fc20148b7ee428b2d332f198b9c
OldSinner <antylifepatia@gmail.com>
2024-08-13 22:47:39 +0200

    Just cam test

--- Detector/main.py
+++ Detector/main.py
@@ -1,4 +1,18 @@
 from detector import *
+import cv2
 
-detector = Detector()
-detector.detect_from_img()
+# detector = Detector()
+# detector.detect_from_img()
+
+cap = cv2.VideoCapture(1)
+frame_rate = 10
+prev = 0
+while True:
+    time_elapsed = time.time() - prev
+    _, frame = cap.read()
+    if time_elapsed > 1./frame_rate:
+        prev = time.time()
+        cv2.imshow("OBS",frame)
+
+        # Do something with your image here.
+   
