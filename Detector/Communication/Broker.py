f1fc066814fe2f4429b810698ae84851ad7570ef
OldSinner <antylifepatia@gmail.com>
2024-08-19 11:10:45 +0200

    Delete aciton handler - no need now

--- Detector/Communication/Broker.py
+++ Detector/Communication/Broker.py
@@ -1,15 +1,15 @@
 import pika
 from Communication.CommunicationModels import *
-from Communication.ActionHandler import *
+from Communication.Logger import LoggerClass
 from Helpers.const import *
 from Configuration.Configuration import *
 
 
 class BrokerReceiver:
-    def __init__(self, detector, config: MQConfiguration):
+    def __init__(self, detector, config: MQConfiguration, logger: LoggerClass):
         self.detector = detector
-        self.actionHandler = ActionHandler(detector)
         self.config = config
+        self.Logger = logger
         self.connection = pika.BlockingConnection(
             pika.ConnectionParameters(self.config.url)
         )
@@ -19,34 +19,46 @@ class BrokerReceiver:
 
     def callback(self, ch, method, properties, body):
         response = GateEvent(body)
-        self.actionHandler.MakeActionOnEvent(response)
+        print(response)
 
     def Consume(self):
-        channel = self.connection.channel()
-        channel.exchange_declare(
-            exchange=self.config.gate_event_queue, exchange_type="fanout"
-        )
-        result = channel.queue_declare(queue="", exclusive=True)
-        queue_name = result.method.queue
-        channel.queue_bind(exchange=self.config.gate_event_queue, queue=queue_name)
-        channel.basic_consume(
-            queue=queue_name, on_message_callback=self.callback, auto_ack=True
-        )
-        channel.start_consuming()
+        try:
+            channel = self.connection.channel()
+            channel.exchange_declare(
+                exchange=self.config.gate_event_queue, exchange_type="fanout"
+            )
+            result = channel.queue_declare(queue="", exclusive=True)
+            queue_name = result.method.queue
+            channel.queue_bind(exchange=self.config.gate_event_queue, queue=queue_name)
+            channel.basic_consume(
+                queue=queue_name, on_message_callback=self.callback, auto_ack=True
+            )
+            self.Logger.LogInfo("BrokerReceiver.Consume", "start_consuming")
+            channel.start_consuming()
+        except Exception as ex:
+            self.Logger.LogErr("BrokerReceiver.Consume", ex)
 
 
 class BrokerSender:
-    def __init__(self, detector, config: MQConfiguration):
+    def __init__(self, detector, config: MQConfiguration, logger: LoggerClass):
         self.config = config
+        self.Logger = logger
         self.connection = pika.BlockingConnection(
             pika.ConnectionParameters(self.config.url)
         )
         self.detector = detector
 
     def SendCloseGateSignal(self):
+        self.Logger.LogInfo(
+            "BrokerSender.SendOpenGateSignal", "Sending signal to close a gate"
+        )
+
         self.SendGateSignal(2)
 
     def SendOpenGateSignal(self):
+        self.Logger.LogInfo(
+            "BrokerSender.SendOpenGateSignal", "Sending signal to open a gate"
+        )
         self.SendGateSignal(1)
 
     def SendGateSignal(self, action):
